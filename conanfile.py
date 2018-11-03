e68201a4d9fbf65055ae468937c95b987318ce35
Eliza Velasquez <4576666+elizagamedev@users.noreply.github.com>
2018-11-03 01:46:58 +0000

    Update Bincrafters conventions

--- conanfile.py
+++ conanfile.py
@@ -5,34 +5,36 @@ import os
 class PhysfsConan(ConanFile):
     name = "physfs"
     version = "3.0.1"
-    license = "ZLIB"
+    description = "Provides abstract access to various archives"
+    topics = ("conan", "libname", "logging")
     url = "https://github.com/bincrafters/conan-physfs"
     homepage = "https://icculus.org/physfs/"
     author = "Bincrafters <bincrafters@gmail.com>"
-    description = "Provides abstract access to various archives"
+    license = "ZLIB"
+    exports = "LICENSE.md"
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake"
+
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
-    generators = "cmake"
-    exports = "LICENSE.md"
-    exports_sources = "CMakeLists.txt"
 
-    source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
 
     def source(self):
         folder = "{}-{}".format(self.name, self.version)
         tools.get("https://icculus.org/physfs/downloads/{}.tar.bz2".format(folder))
-        os.rename(folder, self.source_subfolder)
+        os.rename(folder, self._source_subfolder)
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(build_folder=self.build_subfolder)
+        cmake.configure(build_folder=self._build_subfolder)
         cmake.build()
 
     def package(self):
-        self.copy("LICENSE.txt", dst="licenses", src=self.source_subfolder)
-        self.copy("physfs.h", dst="include", src=os.path.join(self.source_subfolder, "src"))
+        self.copy("LICENSE.txt", dst="licenses", src=self._source_subfolder)
+        self.copy("physfs.h", dst="include", src=os.path.join(self._source_subfolder, "src"))
         if self.options.shared:
             self.copy("*.dll", dst="bin", keep_path=False)
             self.copy("*.lib", dst="lib", keep_path=False, excludes="*-static.lib")
