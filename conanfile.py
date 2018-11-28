7c11f8e4c30532e29bcdf520167da8db15ed8da5
Josh Thompson <jthomp43@uoguelph.ca>
2018-11-28 13:50:48 -0500

    Fixed accidental indent

--- conanfile.py
+++ conanfile.py
@@ -49,7 +49,7 @@ class PhysfsConan(ConanFile):
             self.copy("*.dylib", dst="lib", keep_path=False, symlinks=True)
         else:
             self.copy("*-static.lib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+            self.copy("*.a", dst="lib", keep_path=False)
         self.copy("*.pdb", dst="lib", keep_path=False)
 
     def package_info(self):
